f515ef1793f2d3102ca45b95cd87c09ede80cd8e
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-06-28 22:43:43 +0000

    Quiz 2: Setting up a Database

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,23 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.order("name")
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    if @product.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :cost)
+  end  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+	validates :name, :description, :cost, presence: true	
 end
